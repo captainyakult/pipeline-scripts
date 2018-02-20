87d9d6afc26b29d47426f2e016fb188834d522e7
Stephen Hurley <hurley@jpl.nasa.gov>
2018-02-20 10:36:11 -0800

    Added update_exo for updating the exoplanet database.

---
+++ update_exo.md
@@ -0,0 +1,1 @@
+Updates the exoplanet database, pushes the results to eyes-dev, eyes-staging, and eyes-production, and runs update-manifest on each of the exo-kiosk apps.
---
+++ update_exo.sh
@@ -0,0 +1,13 @@
+$HOME/pipelines/exo-pipeline-scripts/generateEoX_Ranger.sh $HOME/generated-data/exo-pipeline-scripts-data/ http://magic.jpl.nasa.gov:7000/ >> $HOME/logs/exo-pipeline-scripts.log 2>&1
+
+$HOME/pipelines/aws_s3_sync/sync.py upload-folder eyes-dev/ranger/exo-kiosk/static/exo/db $HOME/generated-data/exo-pipeline-scripts-data >> $HOME/logs/aws_s3_sync.log 2>&1
+
+$HOME/pipelines/aws_s3_sync/sync.py update-manifest eyes-dev/ranger/exo-kiosk >> $HOME/logs/aws_s3_sync.log 2>&1
+
+$HOME/pipelines/aws_s3_sync/sync.py upload-folder eyes-staging/ranger/exo-kiosk/static/exo/db $HOME/generated-data/exo-pipeline-scripts-data >> $HOME/logs/aws_s3_sync.log 2>&1
+
+$HOME/pipelines/aws_s3_sync/sync.py update-manifest eyes-staging/ranger/exo-kiosk >> $HOME/logs/aws_s3_sync.log 2>&1
+
+$HOME/pipelines/aws_s3_sync/sync.py upload-folder eyes-production/ranger/exo-kiosk/static/exo/db $HOME/generated-data/exo-pipeline-scripts-data >> $HOME/logs/aws_s3_sync.log 2>&1
+
+$HOME/pipelines/aws_s3_sync/sync.py update-manifest eyes-production/ranger/exo-kiosk >> $HOME/logs/aws_s3_sync.log 2>&1
