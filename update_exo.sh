e74ee5c3611c6ae447304571ef0674c0e6562694
Davit Stepanyan <Davit.Stepanyan@jpl.nasa.gov>
2018-02-28 14:47:56 -0800

    Updating eyes-dev/exo db folder

--- update_exo.sh
+++ update_exo.sh
@@ -11,3 +11,7 @@ $HOME/pipelines/aws_s3_sync/sync.py update-manifest eyes-staging/ranger/exo-kios
 $HOME/pipelines/aws_s3_sync/sync.py upload-folder eyes-production/ranger/exo-kiosk/static/exo/db $HOME/generated-data/exo-pipeline-scripts-data >> $HOME/logs/aws_s3_sync.log 2>&1
 
 $HOME/pipelines/aws_s3_sync/sync.py update-manifest eyes-production/ranger/exo-kiosk >> $HOME/logs/aws_s3_sync.log 2>&1
+
+$HOME/pipelines/aws_s3_sync/sync.py upload-folder eyes-dev/ranger/exo/static/exo/db $HOME/generated-data/exo-pipeline-scripts-data >> $HOME/logs/aws_s3_sync.log 2>&1
+
+$HOME/pipelines/aws_s3_sync/sync.py update-manifest eyes-dev/ranger/exo >> $HOME/logs/aws_s3_sync.log 2>&1
