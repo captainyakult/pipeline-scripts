b000ed4f829c3b7fdfaecbd2ac1262e422afc5c4
Davit Stepanyan <Davit.Stepanyan@jpl.nasa.gov>
2018-03-08 14:19:17 -0800

    Updating exo (addition to exo-kiosk) dbs

--- update_exo.sh
+++ update_exo.sh
@@ -15,3 +15,8 @@ $HOME/pipelines/aws_s3_sync/sync.py update-manifest eyes-production/ranger/exo-k
 $HOME/pipelines/aws_s3_sync/sync.py upload-folder eyes-dev/ranger/exo/static/exo/db $HOME/generated-data/exo-pipeline-scripts-data >> $HOME/logs/aws_s3_sync.log 2>&1
 
 $HOME/pipelines/aws_s3_sync/sync.py update-manifest eyes-dev/ranger/exo >> $HOME/logs/aws_s3_sync.log 2>&1
+
+
+$HOME/pipelines/aws_s3_sync/sync.py upload-folder eyes-dev/ranger/exo/static/exo/db $HOME/generated-data/exo-pipeline-scripts-data >> $HOME/logs/aws_s3_sync.log 2>&1
+
+$HOME/pipelines/aws_s3_sync/sync.py update-manifest eyes-dev/ranger/exo >> $HOME/logs/aws_s3_sync.log 2>&1
