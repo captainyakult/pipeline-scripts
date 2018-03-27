d236afe8dee837b7d0b3a8312e32d01bf0edba64
Stephen Hurley <hurley@jpl.nasa.gov>
2018-03-27 09:45:51 -0700

    Added TLE syncer.

---
+++ tle_syncer.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# tle downloader
+pushd $HOME/pipelines/tle_syncer > /dev/null
+$HOME/pipelines/tle_syncer/sync.py -d $HOME/sources/tle >> $HOME/logs/tle_syncer.log 2>&1
+popd > /dev/null
+
+# copy aws s3
+$HOME/pipelines/aws_s3_sync/sync.py sync-s3-folder eyes-dev/assets/dynamic/tle $HOME/sources/tle >> $HOME/logs/aws_s3_sync.log 2>&1
