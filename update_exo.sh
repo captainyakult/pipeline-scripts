059a4788159cf2c91e248f03d2dae9812cbdaeb0
Davit Stepanyan <Davit.Stepanyan@jpl.nasa.gov>
2018-04-11 12:56:20 -0700

    Adding eyes-dev/assets/dynamic/exo-db

--- update_exo.sh
+++ update_exo.sh
@@ -22,3 +22,9 @@ $HOME/pipelines/aws_s3_sync/sync.py update-manifest eyes-dev/ranger/exo >> $HOME
 $HOME/pipelines/aws_s3_sync/sync.py upload-folder eyes-dev/ranger/exo/static/exo/db $HOME/generated-data/exo-pipeline-scripts-data >> $HOME/logs/aws_s3_sync.log 2>&1
 
 $HOME/pipelines/aws_s3_sync/sync.py update-manifest eyes-dev/ranger/exo >> $HOME/logs/aws_s3_sync.log 2>&1
+
+
+$HOME/pipelines/aws_s3_sync/sync.py upload-folder eyes-dev/assets/dynamic/exo-db $HOME/generated-data/exo-pipeline-scripts-data >> $HOME/logs/aws_s3_sync.log 2>&1
+
+$HOME/pipelines/aws_s3_sync/sync.py update-manifest eyes-dev/assets/dynamic/exo-db >> $HOME/logs/aws_s3_sync.log 2>&1
+
