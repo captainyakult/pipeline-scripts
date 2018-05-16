98b423b856e49de7734c3df7e123c126c6d4cc01
Davit Stepanyan <davit.stepanyan@jpl.nasa.gov>
2018-05-16 13:46:19 -0700

    Switching exo pipeline from magic.jpl.nasa.gov to blackhawk-blade.jpl.nasa.gov

--- update_exo.sh
+++ update_exo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$HOME/pipelines/exo-pipeline-scripts/generateEoX_Ranger.sh $HOME/generated-data/exo-pipeline-scripts-data/ http://magic.jpl.nasa.gov:7000/ >> $HOME/logs/exo-pipeline-scripts.log 2>&1
+$HOME/pipelines/exo-pipeline-scripts/generateEoX_Ranger.sh $HOME/generated-data/exo-pipeline-scripts-data/ http://blackhawk-blade.jpl.nasa.gov:7000/ >> $HOME/logs/exo-pipeline-scripts.log 2>&1
 
 $HOME/pipelines/aws_s3_sync/sync.py upload-folder eyes-dev/ranger/exo-kiosk/static/exo/db $HOME/generated-data/exo-pipeline-scripts-data >> $HOME/logs/aws_s3_sync.log 2>&1
 
