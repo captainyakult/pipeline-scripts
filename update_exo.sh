602f7149851fdfa944b15fdfaa1510d61c61d787
Davit Stepanyan <davit.stepanyan@jpl.nasa.gov>
2018-08-14 11:55:29 -0700

    Fix

--- update_exo.sh
+++ update_exo.sh
@@ -3,11 +3,28 @@
 # fail on any error
 set -e
 
+#production
 $HOME/pipelines/exo-pipeline-scripts/generateEoX_Ranger.sh $HOME/generated-data/exo-pipeline-scripts-data/ http://blackhawk-blade.jpl.nasa.gov:7000/ >> $HOME/logs/exo-pipeline-scripts.log 2>&1
 
 $HOME/pipelines/aws_s3_sync/sync.py upload-folder eyes-production/assets/dynamic/exo/db $HOME/generated-data/exo-pipeline-scripts-data >> $HOME/logs/aws_s3_sync.log 2>&1
 
 $HOME/pipelines/aws_s3_sync/sync.py update-manifest eyes-production/assets/dynamic/exo/db >> $HOME/logs/aws_s3_sync.log 2>&1
+
+
+#staging
+$HOME/pipelines/aws_s3_sync/sync.py upload-folder eyes-staging/assets/dynamic/exo/db $HOME/generated-data/exo-pipeline-scripts-data >> $HOME/logs/aws_s3_sync.log 2>&1
+
+$HOME/pipelines/aws_s3_sync/sync.py update-manifest eyes-staging/assets/dynamic/exo/db >> $HOME/logs/aws_s3_sync.log 2>&1
+
+
+#dev
+$HOME/pipelines/aws_s3_sync/sync.py upload-folder eyes-dev/assets/dynamic/exo/db $HOME/generated-data/exo-pipeline-scripts-data >> $HOME/logs/aws_s3_sync.log 2>&1
+
+$HOME/pipelines/aws_s3_sync/sync.py update-manifest eyes-dev/assets/dynamic/exo/db >> $HOME/logs/aws_s3_sync.log 2>&1
+
+
+
+
 
 
 # old code
--- update_exo_dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-$HOME/pipelines/exo-pipeline-scripts/generateEoX_Ranger.sh $HOME/generated-data/exo-pipeline-scripts-data/ http://blackhawk-blade.jpl.nasa.gov:7000/ >> $HOME/logs/exo-pipeline-scripts.log 2>&1
-
-$HOME/pipelines/aws_s3_sync/sync.py upload-folder eyes-dev/assets/dynamic/exo/db $HOME/generated-data/exo-pipeline-scripts-data >> $HOME/logs/aws_s3_sync.log 2>&1
-
-$HOME/pipelines/aws_s3_sync/sync.py update-manifest eyes-dev/assets/dynamic/exo/db >> $HOME/logs/aws_s3_sync.log 2>&1
