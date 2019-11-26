e44eb51ecbbcbd70907f158249ceb74a046438cb
Stephen Hurley <hurley@jpl.nasa.gov>
2019-11-26 12:53:45 -0800

    Add config folder and made tle_syncer work when not in home.

--- tle_syncer.sh
+++ tle_syncer.sh
@@ -3,13 +3,16 @@
 # fail on any error
 set -e
 
+# Get the base folder of the system.
+BASE=$(cd "$(dirname "$0")/../.."; pwd)
+
 # tle downloader
-pushd $HOME/pipelines/tle_syncer > /dev/null
-$HOME/pipelines/tle_syncer/sync.py -u $HOME/pipelines/configs/tle_urls.txt -n $HOME/pipelines/configs/tle_names.txt -o $HOME/sources/tle/merged.txt >> $HOME/logs/tle_syncer.log 2>&1
+pushd $BASE/pipelines/tle_syncer > /dev/null
+$BASE/pipelines/tle_syncer/sync.py -u $BASE/pipelines/scripts/configs/tle_urls.txt -n $BASE/pipelines/scripts/configs/tle_names.txt -o $BASE/sources/tle/merged.txt >> $BASE/logs/tle_syncer.log 2>&1
 popd > /dev/null
 
 # copy aws s3
-$HOME/pipelines/aws_s3_sync/sync.py sync-s3-folder eyes-dev/assets/dynamic/tle $HOME/sources/tle >> $HOME/logs/aws_s3_sync.log 2>&1
-$HOME/pipelines/aws_s3_sync/sync.py sync-s3-folder eyes-staging/assets/dynamic/tle $HOME/sources/tle >> $HOME/logs/aws_s3_sync.log 2>&1
-$HOME/pipelines/aws_s3_sync/sync.py sync-s3-folder eyes-production/assets/dynamic/tle $HOME/sources/tle >> $HOME/logs/aws_s3_sync.log 2>&1
-$HOME/pipelines/aws_s3_sync/invalidate.py E3JMG193HISS1S "/assets/dynamic/tle/*" >> $HOME/logs/aws_s3_sync.log 2>&1
+$BASE/pipelines/aws_s3_sync/sync.py sync-s3-folder eyes-dev/assets/dynamic/tle $BASE/sources/tle >> $BASE/logs/aws_s3_sync.log 2>&1
+$BASE/pipelines/aws_s3_sync/sync.py sync-s3-folder eyes-staging/assets/dynamic/tle $BASE/sources/tle >> $BASE/logs/aws_s3_sync.log 2>&1
+$BASE/pipelines/aws_s3_sync/sync.py sync-s3-folder eyes-production/assets/dynamic/tle $BASE/sources/tle >> $BASE/logs/aws_s3_sync.log 2>&1
+$BASE/pipelines/aws_s3_sync/invalidate.py E3JMG193HISS1S "/assets/dynamic/tle/*" >> $BASE/logs/aws_s3_sync.log 2>&1
