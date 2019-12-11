9975ca979e2f16fa1abc52766e06f1ad5213e88c
pipeline <pipeline@blackhawk-blade>
2019-12-11 13:33:48 -0800

    Made more pipelines work with new cron_run script.

--- cron_run.sh
+++ cron_run.sh
@@ -10,9 +10,12 @@ LOG_FILE="${LOG_FILE##*/}"
 LOG_FILE="${LOG_FILE%.*}"
 LOG_FILE=$BASE/logs/$LOG_FILE.log
 
+echo "Starting $1..." | $BASE/pipelines/logger/log.sh >> $LOG_FILE
 $1 2>&1 | $BASE/pipelines/logger/log.sh >> $LOG_FILE
 if [ $? -eq 1 ]; then
 	echo "ERROR in script $1" >> $LOG_FILE
 	echo "ERROR in script $1. Please see the log file at $LOG_FILE." | mail -s Error vtad-pipelines@jpl.nasa.gov
 	exit 0
 fi
+echo "Completed $1." | $BASE/pipelines/logger/log.sh >> $LOG_FILE
+
--- update_exo.sh
+++ update_exo.sh
@@ -11,15 +11,13 @@ LOGGER_DIR=$BASE/pipelines/logger
 EXO_DIR=$BASE/sources/exo
 LOGS=$BASE/logs
 
-{
-	# Create the output directory.
-	mkdir -p $EXO_DIR
+# Create the output directory.
+mkdir -p $EXO_DIR
 
-	# Run the exo generator.
-	$EXO_PIPELINE_DIR/generateEoX_Ranger.sh $EXO_DIR/ http://blackhawk-blade.jpl.nasa.gov:7000/
+# Run the exo generator.
+$EXO_PIPELINE_DIR/generateEoX_Ranger.sh $EXO_DIR/ http://blackhawk-blade.jpl.nasa.gov:7000/
 
-	# Upload the files to AWS.
-	$AWS_S3_SYNC_DIR/sync.py sync-s3-folder eyes-dev/assets/dynamic/exo/db $EXO_DIR
-	$AWS_S3_SYNC_DIR/sync.py sync-s3-folder eyes-staging/assets/dynamic/exo/db $EXO_DIR
-	$AWS_S3_SYNC_DIR/sync.py sync-s3-folder eyes-production/assets/dynamic/exo/db $EXO_DIR
-} 2>&1 | $LOGGER_DIR/log.sh $LOGS/update_exo.log
+# Upload the files to AWS.
+$AWS_S3_SYNC_DIR/sync.py sync-s3-folder eyes-dev/assets/dynamic/exo/db $EXO_DIR
+$AWS_S3_SYNC_DIR/sync.py sync-s3-folder eyes-staging/assets/dynamic/exo/db $EXO_DIR
+$AWS_S3_SYNC_DIR/sync.py sync-s3-folder eyes-production/assets/dynamic/exo/db $EXO_DIR
