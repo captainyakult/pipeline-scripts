f6ab371aeed58d33d12c7b97b91a4992d34057c7
pipeline <pipeline@blackhawk-blade>
2020-01-08 13:27:22 -0800

    Made update_spice script more verbose.

--- update_spice.sh
+++ update_spice.sh
@@ -33,27 +33,35 @@ export PATH=/usr/local/gcc-4.9.0/bin:$BASE/pipelines/animdatagen/cspice/exe:$PAT
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/gcc-4.9.0/lib64
 
 # Run spice_syncer.
+echo "  Running spice_syncer..."
 $BASE/pipelines/spice_syncer/sync.py -d $SPICE -u $LOGS/spice_syncer_updated_spice.log
 
 # Run animdatagen.
+echo "  Running animdatagen..."
 pushd $BASE/pipelines/animdatagen > /dev/null
 $BASE/pipelines/animdatagen/animdatagen --update --spice $SPICE --output $ANIMDATA --list $LOGS/spice_syncer_updated_spice.log --updatedAnimdataFile $LOGS/animdatagen_updated_animdata.log
 popd > /dev/null
 
 # Run dynamogen.
+echo "  Running dynamogen..."
 pushd $BASE/pipelines/dynamogen > /dev/null
 $BASE/pipelines/dynamogen/dynamogen --update --spice $SPICE --output $DYNAMO --list $LOGS/spice_syncer_updated_spice.log --updatedFile $LOGS/dynamogen_updated_dynamo.log
 popd > /dev/null
 rm -f $LOGS/spice_syncer_updated_spice.log
 
 # Copy animdata to blackhawk and sync to aws s3.
+echo "  Uploading to animdata to blackhawk2 and S3..."
 while read folder
 do
+	echo "    $folder"
+	echo "      dev"
 	ssh -n -q pipeline@blackhawk2 "mkdir -p /var/server/master/spice/$folder/"
 	scp -r -p -q $ANIMDATA/$folder/* pipeline@blackhawk2:/var/server/master/spice/$folder/
+	echo "      staging"
 	ssh -n -q pipeline@blackhawk2 "mkdir -p /var/server/staging/spice/$folder/"
 	scp -r -p -q $ANIMDATA/$folder/* pipeline@blackhawk2:/var/server/staging/spice/$folder/
 	$AWS_S3_SYNC/sync.py upload-folder eyesstage/server/spice/$folder $ANIMDATA/$folder quiet
+	echo "      production"
 	ssh -n -q pipeline@blackhawk2 "mkdir -p /var/server/production/spice/$folder/"
 	scp -r -p -q $ANIMDATA/$folder/* pipeline@blackhawk2:/var/server/production/spice/$folder/
 	$AWS_S3_SYNC/sync.py upload-folder eyesstatic/server/spice/$folder $ANIMDATA/$folder quiet
@@ -62,11 +70,16 @@ done < $LOGS/animdatagen_updated_animdata.log
 rm -f $LOGS/animdatagen_updated_animdata.log
 
 # sync dynamo to aws s3
+echo "  Uploading dynamo to S3..."
 while read folder
 do
 	folder=`echo $folder | cut -d/ -f1`
+	echo "    $folder"
+	echo "      dev"
 	$AWS_S3_SYNC/sync.py sync-s3-folder eyes-dev/assets/dynamic/dynamo/$folder $DYNAMO/$folder quiet
+	echo "      staging"
 	$AWS_S3_SYNC/sync.py sync-s3-folder eyes-staging/assets/dynamic/dynamo/$folder $DYNAMO/$folder quiet
+	echo "      production"
 	$AWS_S3_SYNC/sync.py sync-s3-folder eyes-production/assets/dynamic/dynamo/$folder $DYNAMO/$folder quiet
 	$AWS_S3_SYNC/invalidate.py $CLOUDFRONT_PRODUCTION_ID "/assets/dynamic/dynamo/"$folder"/*"
 done < $LOGS/dynamogen_updated_dynamo.log
