668f20effbb8423baaf65e605d0071c8a4a581d1
pipeline <pipeline@blackhawk-blade>
2020-07-28 09:53:22 -0700

    Made restart_dsn_xml_pipeline script use proper log.

--- restart_dsn_xml_pipeline.sh
+++ restart_dsn_xml_pipeline.sh
@@ -5,5 +5,6 @@ set -eo pipefail
 
 # Get the base folder of the system.
 BASE=$(cd "$(dirname "$0")/../.."; pwd)
+LOGS=$BASE/logs
 cd $BASE/pipelines/dsn-xml-pipeline
-$BASE/pipelines/dsn-xml-pipeline/restart.sh
+$BASE/pipelines/dsn-xml-pipeline/restart.sh &> $LOGS/dsn_xml_pipeline.log
