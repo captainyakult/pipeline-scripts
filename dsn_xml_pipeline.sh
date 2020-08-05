9e4f10d7d256cb055a7a1388ae08715d9805e33f
pipeline <pipeline@blackhawk-blade>
2020-08-05 08:38:16 -0700

    Added code for clearing out dsn xml pipeline log.

--- dsn_xml_pipeline.sh
+++ dsn_xml_pipeline.sh
@@ -5,5 +5,10 @@ set -eo pipefail
 
 # Get the base folder of the system.
 BASE=$(cd "$(dirname "$0")/../.."; pwd)
+
+# Clear the existing log.
+# rm -f $BASE/logs/dxn_xml_pipeline.log
+
+# Launch the pipeline.
 cd $BASE/pipelines/dsn-xml-pipeline
 $BASE/pipelines/dsn-xml-pipeline/restart.sh
