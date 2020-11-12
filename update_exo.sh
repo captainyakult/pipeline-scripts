9ffb24033775f7b7e96036d0d1793ef847f99a02
Davit Stepanyan <davit.stepanyan@jpl.nasa.gov>
2020-11-12 09:14:21 -0800

    Adding update_exo_dev.sh

--- update_exo.sh
+++ update_exo.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 # Fail on any error.
@@ -5,22 +6,15 @@ set -eo pipefail
 
 # Get the base folder of the system.
 BASE=$(cd "$(dirname "$0")/../.."; pwd)
-EXO_PIPELINE_DIR=$BASE/pipelines/exo-pipeline/script
+EXO_PIPELINE_DIR=$BASE/pipelines/exo-pipeline-scripts
 AWS_S3_SYNC_DIR=$BASE/pipelines/aws-s3-sync
 EXO_DIR=$BASE/sources/exo
 
 # Create the output directory.
 mkdir -p $EXO_DIR
 
-# Updatedatabses.
-$EXO_PIPELINE_DIR/updateDatabases.sh https://eyes.nasa.gov/staging/pipeline/exo
-
 # Run the exo generator.
-$EXO_PIPELINE_DIR/generateEoX_Web.sh $EXO_DIR/ https://eyes.nasa.gov/staging/pipeline/exo
-
-echo $EXO_DIR
+$EXO_PIPELINE_DIR/generateEoX_Ranger.sh $EXO_DIR/ http://blackhawk-blade.jpl.nasa.gov:7000/
 
 # Upload the files to AWS.
-$AWS_S3_SYNC_DIR/sync.py sync-s3-folder eyes-dev/assets/dynamic/exo/db $EXO_DIR
-$AWS_S3_SYNC_DIR/sync.py sync-s3-folder eyes-staging/assets/dynamic/exo/db $EXO_DIR
-# $AWS_S3_SYNC_DIR/sync.py sync-s3-folder eyes-production/assets/dynamic/exo/db $EXO_DIR
+$AWS_S3_SYNC_DIR/sync.py sync-s3-folder eyes-production/assets/dynamic/exo/db $EXO_DIR
