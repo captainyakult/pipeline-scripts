f1f8cb207fb220e357787ef1ca9820af88b07f2a
Davit Stepanyan <davit.stepanyan@jpl.nasa.gov>
2020-11-11 17:40:18 -0800

    Using updateDatabases.sh for exo pipeline

--- update_exo.sh
+++ update_exo.sh
@@ -5,12 +5,15 @@ set -eo pipefail
 
 # Get the base folder of the system.
 BASE=$(cd "$(dirname "$0")/../.."; pwd)
-EXO_PIPELINE_DIR=$BASE/pipelines/exo-pipeline-scripts
+EXO_PIPELINE_DIR=$BASE/pipelines/exo-pipeline/script
 AWS_S3_SYNC_DIR=$BASE/pipelines/aws-s3-sync
 EXO_DIR=$BASE/sources/exo
 
 # Create the output directory.
 mkdir -p $EXO_DIR
+
+# Updatedatabses.
+$EXO_PIPELINE_DIR/updateDatabases.sh https://eyes.nasa.gov/staging/pipeline/exo/
 
 # Run the exo generator.
 $EXO_PIPELINE_DIR/generateEoX_Web.sh $EXO_DIR/ https://eyes.nasa.gov/staging/pipeline/exo/
