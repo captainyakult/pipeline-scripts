dcca53a9b008111d3570aec0e652124e93652baa
Davit Stepanyan <davit.stepanyan@jpl.nasa.gov>
2020-11-11 17:55:44 -0800

    Commenting out exo database pipeline push to production

--- update_exo.sh
+++ update_exo.sh
@@ -21,4 +21,4 @@ $EXO_PIPELINE_DIR/generateEoX_Web.sh $EXO_DIR/ https://eyes.nasa.gov/staging/pip
 # Upload the files to AWS.
 $AWS_S3_SYNC_DIR/sync.py sync-s3-folder eyes-dev/assets/dynamic/exo/db $EXO_DIR
 $AWS_S3_SYNC_DIR/sync.py sync-s3-folder eyes-staging/assets/dynamic/exo/db $EXO_DIR
-$AWS_S3_SYNC_DIR/sync.py sync-s3-folder eyes-production/assets/dynamic/exo/db $EXO_DIR
+# $AWS_S3_SYNC_DIR/sync.py sync-s3-folder eyes-production/assets/dynamic/exo/db $EXO_DIR
