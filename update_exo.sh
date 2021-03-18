b1435533955dd4012ad3db59dd557c07e295ec53
Davit Stepanyan <davit.stepanyan@jpl.nasa.gov>
2021-03-17 17:02:35 -0700

    switching to the new url for new database table

--- update_exo.sh
+++ update_exo.sh
@@ -6,7 +6,7 @@ set -eo pipefail
 
 # Get the base folder of the system.
 BASE=$(cd "$(dirname "$0")/../.."; pwd)
-EXO_PIPELINE_DIR=$BASE/pipelines/exo-pipeline-scripts
+EXO_PIPELINE_DIR=$BASE/pipelines/exo-pipeline/script
 AWS_S3_SYNC_DIR=$BASE/pipelines/aws-s3-sync
 EXO_DIR=$BASE/sources/exo
 
@@ -14,7 +14,7 @@ EXO_DIR=$BASE/sources/exo
 mkdir -p $EXO_DIR
 
 # Run the exo generator.
-$EXO_PIPELINE_DIR/generateEoX_Ranger.sh $EXO_DIR/ http://blackhawk-blade.jpl.nasa.gov:7000/
+$EXO_PIPELINE_DIR/generateEoX_Web.sh $EXO_DIR/ https://eyes.nasa.gov/pipeline/exo
 
 # Upload the files to AWS.
 $AWS_S3_SYNC_DIR/sync.py sync-s3-folder eyes-production/assets/dynamic/exo/db $EXO_DIR
