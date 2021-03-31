7dd2b66be701ac125d3c1f8da14c512af7830469
pipeline <pipeline@blackhawk-blade>
2021-03-31 14:54:54 -0700

    #854 Updated dsn-schedule-pipeline to include uploading to AWS.

--- dsn_schedule_pipeline.sh
+++ dsn_schedule_pipeline.sh
@@ -12,3 +12,8 @@ mkdir -p $BASE/sources/dsn-schedule
 # Launch the pipeline.
 cd $BASE/pipelines/dsn-schedule-pipeline
 python3 $BASE/pipelines/dsn-schedule-pipeline/run.py `date -u +%FT%H` $BASE/sources/dsn-schedule
+
+# Sync the folder to AWS.
+$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-dev/assets/dynamic/dsn-schedule $BASE/sources/dsn-schedule quiet
+$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-staging/assets/dynamic/dsn-schedule $BASE/sources/dsn-schedule quiet
+$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-production/assets/dynamic/dsn-schedule $BASE/sources/dsn-schedule quiet
