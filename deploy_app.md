436e3d91b5c614912ff21be4831d55728b56a215
pipeline <pipeline@blackhawk-blade>
2021-06-08 15:30:39 -0700

    #1068 Changed deploy_app.sh to have more parameters.

--- deploy_app.md
+++ deploy_app.md
@@ -3,11 +3,17 @@
 This script goes through a bunch of steps to deploy an app into the AWS cloud. You can run it like this:
 
 ```
-deploy_app.sh <git repo> <s3 folder> <qa level>
+deploy_app.sh <git repo> <git branch> <build level=dev|production> <deployment folder> <deployment level=dev|staging|production>
 ```
 
-QA level can be either `dev`, `staging`, or `production`. It requires:
+Git repo is the git repo within the ~/deployments folder to use.
+Git branch is the branch of the repo that will be used.
+Build level is either dev or production. It usually is used for choosing a debug or production/minified version of your application.
+Deployment folder is where in the S3 bucket the app will go. It is usually 'apps/<your app name>'.
+Deployment level is either dev, staging, or production. It tells the script which S3 bucket to place the app in.
+
+Before running this script, the app requires:
 
 * A git repo in the `$HOME/deployments/` folder.
-* A build.sh script within the git repo that does whatever is necessary to get files into the `build` folder. The build.sh syntax must look like `build.sh <qa level> <version> <build folder>`. Version is of the format `YYYY-MM-DD.\<qa level>.\<6 chars of last commit>`.
+* A build.sh script within the git repo that does whatever is necessary to get files into the `build` folder. The build.sh syntax must look like `build.sh <qa level> <version> <build folder>`.
 * An existing AWS S3 folder in which the app will be deployed with an existing up-to-date manifest.txt file. It can be generated via `$HOME/pipelines/aws-s3-sync/sync.py update-manifest `eyes-\<QA level>/\<S3 folder name>`
--- deploy_app.sh
+++ deploy_app.sh
@@ -6,29 +6,22 @@ set -eo pipefail
 # Get the base folder of the system.
 BASE=$(cd "$(dirname "$0")/../.."; pwd)
 
-if [[ ($# -ne 3) ]]; then
-	echo "usage: deploy_app.sh <git repo> <s3 folder> <dev|staging|production|custom>"
+if [[ ($# -ne 5) ]]; then
+	echo "usage: deploy_app.sh <git repo> <git branch> <build level=dev|production> <deployment folder> <deployment level=dev|staging|production>"
 	exit 1
 fi
 
 GIT_REPO=$1
-S3_FOLDER_NAME=$2
-QA_LEVEL=$3
+GIT_BRANCH=$2
 BUILD_LEVEL=$3
+DEPLOYMENT_FOLDER_NAME=$4
+DEPLOYMENT_LEVEL=$5
 
 # Switch to the git repo folder.
 pushd $BASE/deployments/$GIT_REPO > /dev/null
 
 # Make sure we're in the right branch and up-to-date.
 echo "Updating the git branch."
-GIT_BRANCH=master
-BUILD_LEVEL=dev
-if [[($QA_LEVEL != "dev")]]; then
-	GIT_BRANCH=$QA_LEVEL
-fi
-if [[($QA_LEVEL = "staging") || ($QA_LEVEL = "production")]]; then
-	BUILD_LEVEL=$QA_LEVEL
-fi
 git fetch -p
 git checkout $GIT_BRANCH
 git reset --hard origin/$GIT_BRANCH || git reset --hard $GIT_BRANCH
@@ -44,25 +37,25 @@ fi
 echo "Using version $VERSION"
 
 # Create the build folder and s3 folder if needed.
-if [ ! -d $BASE/builds/$GIT_REPO-$BUILD_LEVEL/ ]; then
+if [ ! -d $BASE/builds/$GIT_REPO-$GIT_BRANCH-$BUILD_LEVEL/ ]; then
 	echo "Creating build and S3 folders."
-	mkdir -p $BASE/builds/$GIT_REPO-$BUILD_LEVEL/
-	$BASE/pipelines/aws-s3-sync/sync.py update-manifest eyes-$BUILD_LEVEL/$S3_FOLDER_NAME
+	mkdir -p $BASE/builds/$GIT_REPO-$GIT_BRANCH-$BUILD_LEVEL/
+	$BASE/pipelines/aws-s3-sync/sync.py update-manifest eyes-$DEPLOYMENT_LEVEL/$DEPLOYMENT_FOLDER_NAME
 fi
 
 # Build the app, which puts whatever it needs into the builds folder.
 echo "Building the app."
-./build.sh $BUILD_LEVEL $VERSION "$BASE/builds/$GIT_REPO-$BUILD_LEVEL"
+./build.sh $BUILD_LEVEL $VERSION "$BASE/builds/$GIT_REPO-$GIT_BRANCH-$BUILD_LEVEL"
 
 # Generate the version file.
-echo "$VERSION" > $BASE/builds/$GIT_REPO-$BUILD_LEVEL/version.txt
+echo "$VERSION" > $BASE/builds/$GIT_REPO-$GIT_BRANCH-$BUILD_LEVEL/version.txt
 
 # AWS sync the files up to S3.
 echo "Uploading the built app to the S3 folder."
-$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-$BUILD_LEVEL/$S3_FOLDER_NAME $BASE/builds/$GIT_REPO-$BUILD_LEVEL
+$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-$DEPLOYMENT_LEVEL/$DEPLOYMENT_FOLDER_NAME $BASE/builds/$GIT_REPO-$GIT_BRANCH-$BUILD_LEVEL
 
-if [[($BUILD_LEVEL = "production")]]; then
-	$BASE/pipelines/aws-s3-sync/invalidate.py E3JMG193HISS1S "/"$S3_FOLDER_NAME"/*"
+if [[($DEPLOYMENT_LEVEL = "production")]]; then
+	$BASE/pipelines/aws-s3-sync/invalidate.py E3JMG193HISS1S "/"$DEPLOYMENT_FOLDER_NAME"/*"
 fi
 
 # Switch back to the called folder.
