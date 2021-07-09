bf0e1fd5cbf108a3aa8f1e308378f42f2275f31f
Stephen Hurley <hurley@jpl.nasa.gov>
2021-07-09 08:54:34 -0700

    #1093 Updated asteroid pipeline to include spice and dynamo generation.

--- asteroid_pipeline.md
+++ asteroid_pipeline.md
@@ -1,3 +1,3 @@
 # asteroid-pipeline
 
-It runs the asteroid-pipeline, which outputs some .dat and .json files. It then moves them to the proper $HOME/sources/asteroids/ folder and then uploads it all to AWS dev, staging, and production.
+It runs the asteroid-pipeline, which outputs some .dat, .json, and .txt files to the $BASE/sources/asteroids and $BASE/sources/spice/asteroids folders. Then it generates dynamo and uploads it all to AWS dev, staging, and production.
--- asteroid_pipeline.sh
+++ asteroid_pipeline.sh
@@ -8,14 +8,37 @@ BASE=$(cd "$(dirname "$0")/../.."; pwd)
 
 # Launch the pipeline.
 cd $BASE/pipelines/asteroids-pipeline
-python3 sync.py
+python3 sync.py -v -o $BASE/sources/asteroids -os $BASE/sources/spice
 
-# Create the folder if it doesn't already exist.
-mkdir -p $BASE/sources/asteroids
-
-# Move the generated files to the sources folder.
-mv neos.* $BASE/sources/asteroids/
-mv closest_approaches.*.json $BASE/sources/asteroids/
+# For every new spk, generate dynamo.
+while read F; do
+        IFS=',' read -ra ITEMS <<< "$F"
+		# Get the item info.
+		PIONEER_NAME=${ITEMS[0]}
+		NAIF_ID=${ITEMS[1]}
+		echo "Generating dynamo for $PIONEER_NAME..."
+		# Create the dynamo config.
+		cat > $BASE/sources/asteroids/$PIONEER_NAME.sun.orb.json <<- EOM
+		{
+			"type" : "orb",
+			"body" : $NAIF_ID,
+			"otherBody" : 10,
+			"spiceDirs" : [
+				"lsk",
+				"celestial",
+				"$PIONEER_NAME"
+			],
+			"limitedCoverage" : [null, null]
+		}
+		EOM
+		# Run dynamo.
+		$BASE/pipelines/dynamogen/dynamogen --spice $BASE/sources/spice --output $BASE/sources/dynamo --config $BASE/sources/asteroids/$PIONEER_NAME.sun.orb.json
+		# Sync the dynamo folder to AWS.
+		$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-dev/assets/dynamic/dynamo/$PIONEER_NAME $BASE/sources/dynamo/$PIONEER_NAME quiet
+		$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-staging/assets/dynamic/dynamo/$PIONEER_NAME $BASE/sources/dynamo/$PIONEER_NAME quiet
+		$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-production/assets/dynamic/dynamo/$PIONEER_NAME $BASE/sources/dynamo/$PIONEER_NAME quiet
+		$BASE/pipelines/aws-s3-sync/invalidate.py E3JMG193HISS1S "/assets/dynamic/dynamo/$PIONEER_NAME/*"
+done < $BASE/sources/asteroids/newly_generated_spks.txt
 
 # Sync the folder to AWS.
 $BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-dev/assets/dynamic/asteroids $BASE/sources/asteroids quiet
