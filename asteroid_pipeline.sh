072208cdeb2f5c6696b07222abc2daedbd1ec31b
Stephen Hurley <hurley@jpl.nasa.gov>
2021-10-08 12:14:12 -0700

    #1093 +p Made asteroids pipeline output to small_solar_system_bodies folder.

--- asteroid_pipeline.sh
+++ asteroid_pipeline.sh
@@ -8,7 +8,7 @@ BASE=$(cd "$(dirname "$0")/../.."; pwd)
 
 # Launch the pipeline. It will also create closest_approaches.txt and newly_generated_spks.txt file for creating dynamos.
 cd $BASE/pipelines/asteroids-pipeline
-python3 sync.py -v -o $BASE/sources/asteroids -os $BASE/sources/spice
+python3 sync_neos.py -v -o $BASE/sources/asteroids -os $BASE/sources/spice/small_solar_system_bodies
 
 # For every new closest approach in the closest_approaches file, generate dynamo.
 while read F; do
@@ -20,7 +20,7 @@ while read F; do
 		END_TIME=${ITEMS[3]}
 		echo "Generating closest approach dynamo for $PIONEER_NAME..."
 		# Create the dynamo config.
-		cat > $BASE/sources/asteroids/$PIONEER_NAME.earth.orb.json <<- EOM
+		cat > $BASE/sources/asteroids/small_solar_system_bodies.$PIONEER_NAME.earth.orb.json <<- EOM
 		{
 			"type" : "orb",
 			"body" : $NAIF_ID,
@@ -28,20 +28,20 @@ while read F; do
 			"spiceDirs" : [
 				"lsk",
 				"celestial",
-				"$PIONEER_NAME"
+				"small_solar_system_bodies/$PIONEER_NAME"
 			],
 			"limitedCoverage" : [$START_TIME, $END_TIME]
 		}
 		EOM
 		# Run dynamo.
-		$BASE/pipelines/dynamogen/dynamogen --spice $BASE/sources/spice --output $BASE/sources/dynamo --config $BASE/sources/asteroids/$PIONEER_NAME.earth.orb.json
-		rm $BASE/sources/asteroids/$PIONEER_NAME.earth.orb.json
+		$BASE/pipelines/dynamogen/dynamogen --spice $BASE/sources/spice --output $BASE/sources/dynamo --config $BASE/sources/asteroids/small_solar_system_bodies.$PIONEER_NAME.earth.orb.json
+		rm $BASE/sources/asteroids/small_solar_system_bodies.$PIONEER_NAME.earth.orb.json
 		# Sync the dynamo folder to AWS.
 		echo "  Uploading to dev, staging, and production AWS..."
-		$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-dev/assets/dynamic/dynamo/$PIONEER_NAME $BASE/sources/dynamo/$PIONEER_NAME quiet
-		$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-staging/assets/dynamic/dynamo/$PIONEER_NAME $BASE/sources/dynamo/$PIONEER_NAME quiet
-		$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-production/assets/dynamic/dynamo/$PIONEER_NAME $BASE/sources/dynamo/$PIONEER_NAME quiet
-		$BASE/pipelines/aws-s3-sync/invalidate.py E3JMG193HISS1S "/assets/dynamic/dynamo/$PIONEER_NAME/*"
+		$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-dev/assets/dynamic/dynamo/small_solar_system_bodies/$PIONEER_NAME $BASE/sources/dynamo/small_solar_system_bodies/$PIONEER_NAME quiet
+		$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-staging/assets/dynamic/dynamo/small_solar_system_bodies/$PIONEER_NAME $BASE/sources/dynamo/small_solar_system_bodies/$PIONEER_NAME quiet
+		$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-production/assets/dynamic/dynamo/small_solar_system_bodies/$PIONEER_NAME $BASE/sources/dynamo/small_solar_system_bodies/$PIONEER_NAME quiet
+		$BASE/pipelines/aws-s3-sync/invalidate.py E3JMG193HISS1S "/assets/dynamic/dynamo/small_solar_system_bodies/$PIONEER_NAME/*"
 done < $BASE/sources/asteroids/closest_approaches.txt
 
 # Delete the no longer needed closest_approaches file.
@@ -59,7 +59,7 @@ if [[ -f "$BASE/sources/asteroids/newly_generated_spks.txt" ]]; then
 			NAIF_ID=${ITEMS[1]}
 			echo "Generating dynamo for $PIONEER_NAME..."
 			# Create the dynamo config.
-			cat > $BASE/sources/asteroids/$PIONEER_NAME.sun.orb.json <<- EOM
+			cat > $BASE/sources/asteroids/small_solar_system_bodies.$PIONEER_NAME.sun.orb.json <<- EOM
 			{
 				"type" : "orb",
 				"body" : $NAIF_ID,
@@ -67,20 +67,20 @@ if [[ -f "$BASE/sources/asteroids/newly_generated_spks.txt" ]]; then
 				"spiceDirs" : [
 					"lsk",
 					"celestial",
-					"$PIONEER_NAME"
+					"small_solar_system_bodies/$PIONEER_NAME"
 				],
 				"limitedCoverage" : [null, null]
 			}
 			EOM
 			# Run dynamo.
-			$BASE/pipelines/dynamogen/dynamogen --spice $BASE/sources/spice --output $BASE/sources/dynamo --config $BASE/sources/asteroids/$PIONEER_NAME.sun.orb.json
-			rm $BASE/sources/asteroids/$PIONEER_NAME.sun.orb.json
+			$BASE/pipelines/dynamogen/dynamogen --spice $BASE/sources/spice --output $BASE/sources/dynamo --config $BASE/sources/asteroids/small_solar_system_bodies.$PIONEER_NAME.sun.orb.json
+			rm $BASE/sources/asteroids/small_solar_system_bodies.$PIONEER_NAME.sun.orb.json
 			# Sync the dynamo folder to AWS.
 			echo "  Uploading to dev, staging, and production AWS..."
-			$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-dev/assets/dynamic/dynamo/$PIONEER_NAME $BASE/sources/dynamo/$PIONEER_NAME quiet
-			$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-staging/assets/dynamic/dynamo/$PIONEER_NAME $BASE/sources/dynamo/$PIONEER_NAME quiet
-			$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-production/assets/dynamic/dynamo/$PIONEER_NAME $BASE/sources/dynamo/$PIONEER_NAME quiet
-			$BASE/pipelines/aws-s3-sync/invalidate.py E3JMG193HISS1S "/assets/dynamic/dynamo/$PIONEER_NAME/*"
+			$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-dev/assets/dynamic/dynamo/small_solar_system_bodies/$PIONEER_NAME $BASE/sources/dynamo/small_solar_system_bodies/$PIONEER_NAME quiet
+			$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-staging/assets/dynamic/dynamo/small_solar_system_bodies/$PIONEER_NAME $BASE/sources/dynamo/small_solar_system_bodies/$PIONEER_NAME quiet
+			$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-production/assets/dynamic/dynamo/small_solar_system_bodies/$PIONEER_NAME $BASE/sources/dynamo/small_solar_system_bodies/$PIONEER_NAME quiet
+			$BASE/pipelines/aws-s3-sync/invalidate.py E3JMG193HISS1S "/assets/dynamic/dynamo/small_solar_system_bodies/$PIONEER_NAME/*"
 	done < $BASE/sources/asteroids/newly_generated_spks.txt
 fi
 
