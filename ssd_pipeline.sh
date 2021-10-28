dc0ceea9e3a31bf7b8ee8d5133efde5a71dadc00
Stephen Hurley <hurley@jpl.nasa.gov>
2021-10-28 08:47:34 -0700

    #1765 Moved ssd-pipeline output to ssd folders.

--- ssd_pipeline.sh
+++ ssd_pipeline.sh
@@ -8,11 +8,11 @@ BASE=$(cd "$(dirname "$0")/../.."; pwd)
 
 # Launch the pipeline. It will also create newly_generated_spks.txt file for creating dynamos.
 cd $BASE/pipelines/ssd-pipeline
-python3 sync_neos.py -v -o $BASE/sources/asteroids -os $BASE/sources/spice/small_solar_system_bodies
+python3 sync.py -v -o $BASE/sources/ssd -os $BASE/sources/spice/ssd
 
-if [[ -f "$BASE/sources/asteroids/newly_generated_spks.txt" ]]; then
+if [[ -f "$BASE/sources/ssd/newly_generated_spks.txt" ]]; then
 	# Sort and make unique the newly_generated_spks file since it may have picked up multiple runs before being used and removed.
-	sort -u -o $BASE/sources/asteroids/newly_generated_spks.txt $BASE/sources/asteroids/newly_generated_spks.txt
+	sort -u -o $BASE/sources/ssd/newly_generated_spks.txt $BASE/sources/ssd/newly_generated_spks.txt
 
 	# For every new spk in the newly_generated_spks file, generate dynamo.
 	while read F; do
@@ -24,7 +24,7 @@ if [[ -f "$BASE/sources/asteroids/newly_generated_spks.txt" ]]; then
 			CA_DIST=${ITEMS[3]}
 			echo "Generating dynamo for $PIONEER_NAME..."
 			# Create the dynamo config.
-			cat > $BASE/sources/asteroids/small_solar_system_bodies.$PIONEER_NAME.sun.orb.json <<- EOM
+			cat > $BASE/sources/ssd/ssd.$PIONEER_NAME.sun.orb.json <<- EOM
 			{
 				"type" : "orb",
 				"body" : $NAIF_ID,
@@ -32,14 +32,14 @@ if [[ -f "$BASE/sources/asteroids/newly_generated_spks.txt" ]]; then
 				"spiceDirs" : [
 					"lsk",
 					"celestial",
-					"small_solar_system_bodies/$PIONEER_NAME"
+					"ssd/$PIONEER_NAME"
 				],
 				"limitedCoverage" : [null, null]
 			}
 			EOM
 			# Run dynamo.
-			$BASE/pipelines/dynamogen/dynamogen --spice $BASE/sources/spice --output $BASE/sources/dynamo --config $BASE/sources/asteroids/small_solar_system_bodies.$PIONEER_NAME.sun.orb.json
-			rm $BASE/sources/asteroids/small_solar_system_bodies.$PIONEER_NAME.sun.orb.json
+			$BASE/pipelines/dynamogen/dynamogen --spice $BASE/sources/spice --output $BASE/sources/dynamo --config $BASE/sources/ssd/ssd.$PIONEER_NAME.sun.orb.json
+			rm $BASE/sources/ssd/ssd.$PIONEER_NAME.sun.orb.json
 			# If the next closest approach distance is less than 0.05 AU, make earth-relative dynamo for that approach as well.
 			if (( $(echo "$CA_DIST < 7479893.535" | bc -l) )); then
 				# Do the same for earth.
@@ -48,7 +48,7 @@ if [[ -f "$BASE/sources/asteroids/newly_generated_spks.txt" ]]; then
 				START_TIME=$(echo "$CA_TIME - 604800" | bc)
 				END_TIME=$(echo "$CA_TIME + 604800" | bc)
 				# Create the dynamo config.
-				cat > $BASE/sources/asteroids/small_solar_system_bodies.$PIONEER_NAME.earth.orb.json <<- EOM
+				cat > $BASE/sources/ssd/ssd.$PIONEER_NAME.earth.orb.json <<- EOM
 				{
 					"type" : "orb",
 					"body" : $NAIF_ID,
@@ -56,30 +56,29 @@ if [[ -f "$BASE/sources/asteroids/newly_generated_spks.txt" ]]; then
 					"spiceDirs" : [
 						"lsk",
 						"celestial",
-						"small_solar_system_bodies/$PIONEER_NAME"
+						"ssd/$PIONEER_NAME"
 					],
 					"limitedCoverage" : [$START_TIME, $END_TIME]
 				}
 				EOM
 				# Run dynamo.
-				$BASE/pipelines/dynamogen/dynamogen --spice $BASE/sources/spice --output $BASE/sources/dynamo --config $BASE/sources/asteroids/small_solar_system_bodies.$PIONEER_NAME.earth.orb.json
-				rm $BASE/sources/asteroids/small_solar_system_bodies.$PIONEER_NAME.earth.orb.json
+				$BASE/pipelines/dynamogen/dynamogen --spice $BASE/sources/spice --output $BASE/sources/dynamo --config $BASE/sources/ssd/ssd.$PIONEER_NAME.earth.orb.json
+				rm $BASE/sources/ssd/ssd.$PIONEER_NAME.earth.orb.json
 			fi
 			# Sync the dynamo folder to AWS.
 			echo "  Uploading to dev, staging, and production AWS..."
-			$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-dev/assets/dynamic/dynamo/small_solar_system_bodies/$PIONEER_NAME $BASE/sources/dynamo/small_solar_system_bodies/$PIONEER_NAME quiet
-			$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-staging/assets/dynamic/dynamo/small_solar_system_bodies/$PIONEER_NAME $BASE/sources/dynamo/small_solar_system_bodies/$PIONEER_NAME quiet
-			$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-production/assets/dynamic/dynamo/small_solar_system_bodies/$PIONEER_NAME $BASE/sources/dynamo/small_solar_system_bodies/$PIONEER_NAME quiet
-			$BASE/pipelines/aws-s3-sync/invalidate.py E3JMG193HISS1S "/assets/dynamic/dynamo/small_solar_system_bodies/$PIONEER_NAME/*"
-	done < $BASE/sources/asteroids/newly_generated_spks.txt
+			$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-dev/assets/dynamic/dynamo/ssd/$PIONEER_NAME $BASE/sources/dynamo/ssd/$PIONEER_NAME quiet
+			$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-staging/assets/dynamic/dynamo/ssd/$PIONEER_NAME $BASE/sources/dynamo/ssd/$PIONEER_NAME quiet
+			$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-production/assets/dynamic/dynamo/ssd/$PIONEER_NAME $BASE/sources/dynamo/ssd/$PIONEER_NAME quiet
+			$BASE/pipelines/aws-s3-sync/invalidate.py E3JMG193HISS1S "/assets/dynamic/dynamo/ssd/$PIONEER_NAME/*"
+	done < $BASE/sources/ssd/newly_generated_spks.txt
 fi
 
 # Delete the no longer needed newly_generated_spks file.
-rm -f $BASE/sources/asteroids/newly_generated_spks.txt
+rm -f $BASE/sources/ssd/newly_generated_spks.txt
 
-# Sync the asteroids folder to AWS.
-$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-dev/assets/dynamic/asteroids $BASE/sources/asteroids quiet
-$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-staging/assets/dynamic/asteroids $BASE/sources/asteroids quiet
-$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-production/assets/dynamic/asteroids $BASE/sources/asteroids quiet
-$BASE/pipelines/aws-s3-sync/invalidate.py E3JMG193HISS1S "/assets/dynamic/asteroids/*"
-
+# Sync the ssd folder to AWS.
+$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-dev/assets/dynamic/ssd $BASE/sources/ssd quiet
+$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-staging/assets/dynamic/ssd $BASE/sources/ssd quiet
+$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-production/assets/dynamic/ssd $BASE/sources/ssd quiet
+$BASE/pipelines/aws-s3-sync/invalidate.py E3JMG193HISS1S "/assets/dynamic/ssd/*"
