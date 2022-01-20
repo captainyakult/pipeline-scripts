a5f61201b62af397dbcc3dd4010b2d4f34a2d7a3
Stephen Hurley <hurley@jpl.nasa.gov>
2022-01-20 11:07:02 -0800

    #2120 Added initial position pipeline script.

---
+++ position_pipeline.md
@@ -0,0 +1,1 @@
+A pipeline that generates the positions of various entities relative to other entities.
---
+++ position_pipeline.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Fail on any error.
+set -eo pipefail
+
+# Get the base folder of the system.
+BASE=$(cd "$(dirname "$0")/../.."; pwd)
+
+SPICE=$BASE/sources/spice
+POSITIONS=$BASE/sources/positions
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/gcc-4.9.0/lib64
+
+# Run position_pipeline.
+pushd $BASE/pipelines/position-pipeline > /dev/null
+$BASE/pipelines/position-pipeline/position_pipeline -s $SPICE -o $POSITIONS -e jpl -r solar_system_barycenter
+popd > /dev/null
+
