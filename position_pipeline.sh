e48f5120c097789e42daa56f8088b63907217e50
Stephen Hurley <hurley@jpl.nasa.gov>
2022-01-20 11:12:12 -0800

    #2120 Renamed jpl to place_jpl in positions_pipeline script.

--- position_pipeline.sh
+++ position_pipeline.sh
@@ -13,6 +13,6 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/gcc-4.9.0/lib64
 
 # Run position_pipeline.
 pushd $BASE/pipelines/position-pipeline > /dev/null
-$BASE/pipelines/position-pipeline/position_pipeline -s $SPICE -o $POSITIONS -e jpl -r solar_system_barycenter
+$BASE/pipelines/position-pipeline/position_pipeline -s $SPICE -o $POSITIONS -e place_jpl -r solar_system_barycenter
 popd > /dev/null
 
