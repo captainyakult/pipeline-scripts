889a94fe635153c8c18841bdb03cf81e2d84f961
pipeline <pipeline@blackhawk3>
2022-03-15 13:51:30 -0700

    #2212 Got cron_run working.

--- cron_run.sh
+++ cron_run.sh
@@ -3,9 +3,6 @@
 # Fail on any error.
 set -o pipefail
 
-# Set the environment.
-export PATH=$HOME/pipelines/_external/cspice/exe:/usr/local/bin:$PATH
-
 BASE=$(cd "$(dirname "$0")/../.."; pwd)
 
 # Store and remove the first param as the command from the rest so that $@ works.
@@ -25,17 +22,18 @@ LOG_FILE=$BASE/logs/$LOG_FILE.log # prepend the log path
 export REQUESTS_CA_BUNDLE=$HOME/cert.pem
 export SSL_CERT_FILE=$HOME/cert.pem
 
-echo "Starting $COMMAND..." | $BASE/pipelines/logger/log.sh >> $LOG_FILE
-if [[ $2 == "bg" ]]; then
-	shift
-	$COMMAND "$@" 2>&1 | $BASE/pipelines/logger/log.sh >> $LOG_FILE &
-else
-	$COMMAND "$@" 2>&1 | $BASE/pipelines/logger/log.sh >> $LOG_FILE
-fi
+function run {
+	"$@" 2>&1 | awk '{ print strftime("%Y-%m-%d %H:%M:%S"), $0; fflush(); }' >> $LOG_FILE
+}
+
+run echo Starting $COMMAND
+
+run $COMMAND "$@"
+
 if [ $? -ne 0 ]; then
-	echo "ERROR in script $COMMAND" >> $LOG_FILE
+	run echo "ERROR in script $COMMAND"
 	echo "ERROR in script $COMMAND. Please see the log file at $LOG_FILE." | mail -s Error vtad-pipelines@jpl.nasa.gov
 	exit 0
 fi
-echo "Completed $COMMAND." | $BASE/pipelines/logger/log.sh >> $LOG_FILE
+run echo Completed $COMMAND.
 
