ce9a10b8a91b5c006eeb2670a41dc022584e1647
pipeline <pipeline@blackhawk3>
2022-03-22 12:59:00 -0700

    #2212 Got dsn-xml-pipelinie fully on bh3.

--- dsn_schedule_pipeline.sh
+++ dsn_schedule_pipeline.sh
@@ -3,17 +3,13 @@
 # Fail on any error.
 set -eo pipefail
 
-# Get the base folder of the system.
-BASE=$(cd "$(dirname "$0")/../.."; pwd)
-
 # Create the folder if it doesn't already exist.
-mkdir -p $BASE/sources/dsn-schedule
+mkdir -p $BASE/data/dsn-schedule
 
 # Launch the pipeline.
-cd $BASE/pipelines/dsn-schedule-pipeline
-python3 $BASE/pipelines/dsn-schedule-pipeline/run.py `date -u +%FT%H` $BASE/sources/dsn-schedule
+$BASE/code/dsn-schedule-pipeline/run.sh `date -u +%FT%H` $BASE/data/dsn-schedule
 
 # Sync the folder to AWS.
-$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-dev/assets/dynamic/dsn-schedule $BASE/sources/dsn-schedule quiet
-$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-staging/assets/dynamic/dsn-schedule $BASE/sources/dsn-schedule quiet
-$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-production/assets/dynamic/dsn-schedule $BASE/sources/dsn-schedule quiet
+$BASE/code/aws-s3-sync/sync.sh sync-s3-folder eyes-dev/assets/dynamic/dsn-schedule $BASE/data/dsn-schedule quiet
+$BASE/code/aws-s3-sync/sync.sh sync-s3-folder eyes-staging/assets/dynamic/dsn-schedule $BASE/data/dsn-schedule quiet
+$BASE/code/aws-s3-sync/sync.sh sync-s3-folder eyes-production/assets/dynamic/dsn-schedule $BASE/data/dsn-schedule quiet
