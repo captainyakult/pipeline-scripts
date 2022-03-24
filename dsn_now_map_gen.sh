5c005edaead44052cb65f94c0fa993e21984dcf9
pipeline <pipeline@blackhawk3>
2022-03-24 05:05:42 -0700

    #2212 Got dsn-now-map-gen working on bh3.

--- dsn_now_map_gen.sh
+++ dsn_now_map_gen.sh
@@ -6,20 +6,14 @@ set -eo pipefail
 # Get the base folder of the system.
 BASE=$(cd "$(dirname "$0")/../.."; pwd)
 
-# make sure the library path is set correctly
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/gcc-4.9.0/lib64
+# Make sure the data folder exists.
+mkdir -p $BASE/data/dsn-now-map
 
-# make sure the output folder exists
-mkdir -p $BASE/pipelines/dsn-now-map-gen/output
+# Run the pipeline.
+$BASE/code/dsn-now-map-gen/run.sh $BASE/data/dsn-now-map
 
-# create the map files
-pushd $BASE/pipelines/dsn-now-map-gen > /dev/null
-./updatemap
-popd > /dev/null
-
-# copy aws s3 (no logging since it happens so often)
-$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-dev/assets/dynamic/dsn-now-map $BASE/pipelines/dsn-now-map-gen/output quiet
-$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-staging/assets/dynamic/dsn-now-map $BASE/pipelines/dsn-now-map-gen/output quiet
-$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-production/assets/dynamic/dsn-now-map $BASE/pipelines/dsn-now-map-gen/output quiet
-
+# Sync the data folder to AWS.
+$BASE/code/aws-s3-sync/sync.sh sync-s3-folder eyes-dev/assets/dynamic/dsn-now-map $BASE/data/dsn-now-map quiet
+$BASE/code/aws-s3-sync/sync.sh sync-s3-folder eyes-staging/assets/dynamic/dsn-now-map $BASE/data/dsn-now-map quiet
+$BASE/code/aws-s3-sync/sync.sh sync-s3-folder eyes-production/assets/dynamic/dsn-now-map $BASE/data/dsn-now-map quiet
 
