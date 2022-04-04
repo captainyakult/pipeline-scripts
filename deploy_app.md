594bb79fb92fabd4d8cef81d5bec40ffe6d5c2c4
pipeline <pipeline@blackhawk3>
2022-04-04 12:36:27 -0700

    #2212 Dockerized deploy_app.sh.

--- deploy_app.md
+++ deploy_app.md
@@ -3,17 +3,18 @@
 This script goes through a bunch of steps to deploy an app into the AWS cloud. You can run it like this:
 
 ```
-deploy_app.sh <git repo> <git branch> <build level=dev|production> <deployment folder> <deployment level=local|dev|staging|production>
+deploy_app.sh <git repo> <git branch> <build level=dev|production> <deployment folder> <deployment level=local|dev|staging|production> [addversion]
 ```
 
-* `Git repo` is the git repo within the `~/deployments` folder to use.
+* `Git repo` is the git repo within the `$BASE/data/deployments` folder to use.
 * `Git branch` is the branch of the repo that will be used.
 * `Build level` is either dev or production. It is used for choosing a debug or production/minified version of your application.
-* `Deployment folder` is where in the S3 bucket (for non-local) or `~/www` folder (for local) the app will go. It is usually `apps/<your app name>`.
-* `Deployment level` is either local, dev, staging, or production. If local, it will place the app at `~/www` on the server. Otherwise it tells the script which S3 bucket to place the app in (`eyes-dev`, `eyes-staging`, or `eyes-production`).
+* `Deployment folder` is where in the S3 bucket (for non-local) or `$BASE/data/www` folder (for local) the app will go. It is usually `apps/<your app name>`.
+* `Deployment level` is either local, dev, staging, or production. If local, it will place the app at `$BASE/data/www` on the server. Otherwise it tells the script which S3 bucket to place the app in (`eyes-dev`, `eyes-staging`, or `eyes-production`).
+* `addversion`, if specified, will also deploy to to `<deployment folder>-<version>/` in addition to the deployment folder.
 
 Before running this script, the app requires:
 
-* A git repo in the `$HOME/deployments/` folder.
+* A git repo in the `$BASE/data/deployments/` folder.
+* A Dockerfile within the git repo that does whatever is necessary to properly run the build script in a docker image. The $BASE file system will be mounted at its same place within the container, and the Docker container will run as pipeline (same UID).
 * A build.sh script within the git repo that does whatever is necessary to get files into the `build` folder. The build.sh syntax must look like `build.sh <qa level> <version> <build folder>`.
-* An existing AWS S3 folder in which the app will be deployed with an existing up-to-date manifest.txt file if `dev`, `staging`, or `production` is given for the deployment level. It can be generated via `$HOME/pipelines/aws-s3-sync/sync.py update-manifest `eyes-\<QA level>/\<S3 folder name>`
--- deploy_app.sh
+++ deploy_app.sh
@@ -6,8 +6,8 @@ set -eo pipefail
 # Get the base folder of the system.
 BASE=$(cd "$(dirname "$0")/../.."; pwd)
 
-if [[ ($# -ne 5) ]]; then
-	echo "usage: deploy_app.sh <git repo> <git branch> <build level=dev|production> <deployment folder> <deployment level=local|dev|staging|production>"
+if [[ ($# -lt 5) ]]; then
+	echo "usage: deploy_app.sh <git repo> <git branch> <build level=dev|production> <deployment folder> <deployment level=local|dev|staging|production> [addversion]"
 	exit 1
 fi
 
@@ -16,9 +16,22 @@ GIT_BRANCH=$2
 BUILD_LEVEL=$3
 DEPLOYMENT_FOLDER_NAME=$4
 DEPLOYMENT_LEVEL=$5
+ADD_VERSION=$6
+
+# Make sure the deployment level is valid.
+if ! [[ $DEPLOYMENT_LEVEL =~ ^(local|dev|staging|production)$ ]]; then
+	echo "The deployment level may either be local, dev, staging, or production."
+	exit 1
+fi
+
+# Make sure the deployment folder name is valid.
+if ! [[ $DEPLOYMENT_FOLDER_NAME =~ ^(apps|libs|(assets/static)).* ]]; then
+	echo "If installing locally, you must choose a deployment folder that starts with 'apps', 'libs', or 'assets/static'."
+	exit 1
+fi
 
 # Switch to the git repo folder.
-pushd $BASE/deployments/$GIT_REPO > /dev/null
+pushd $BASE/data/deployments/$GIT_REPO > /dev/null
 
 # Make sure we're in the right branch and up-to-date.
 echo "Updating the git branch."
@@ -27,7 +40,7 @@ git checkout $GIT_BRANCH
 git reset --hard origin/$GIT_BRANCH || git reset --hard $GIT_BRANCH
 
 # Generate the version.
-VERSION=$((node -p "require('./package.json').version" || true) 2> /dev/null)
+VERSION=$((grep "\"version\"\w*:" package.json | awk -F'"' '{print $4}' || true) 2> /dev/null)
 if [[($VERSION = "")]]; then
 	VERSION=$((cat version.txt || true) 2> /dev/null)
 fi
@@ -37,38 +50,46 @@ fi
 echo "Using version $VERSION"
 
 # Create the build folder and s3 folder if needed.
-if [ ! -d $BASE/builds/$GIT_REPO-$DEPLOYMENT_LEVEL/ ]; then
+if [ ! -d $BASE/temp/build-$GIT_REPO-$DEPLOYMENT_LEVEL/ ]; then
 	echo "Creating build folder."
-	mkdir -p $BASE/builds/$GIT_REPO-$DEPLOYMENT_LEVEL/
-	if [[($DEPLOYMENT_LEVEL != "local")]]; then
-		echo "Creating S3 folder."
-		$BASE/pipelines/aws-s3-sync/sync.py update-manifest eyes-$DEPLOYMENT_LEVEL/$DEPLOYMENT_FOLDER_NAME
-	fi
+	mkdir -p $BASE/temp/build-$GIT_REPO-$DEPLOYMENT_LEVEL/
 fi
 
-# Build the app, which puts whatever it needs into the builds folder.
-echo "Building the app."
-./build.sh $BUILD_LEVEL $VERSION "$BASE/builds/$GIT_REPO-$DEPLOYMENT_LEVEL"
+# Build docker image.
+echo "Building the docker image."
+docker build -t $GIT_REPO . > /dev/null
+
+# Run the docker image as a container.
+echo "Building the app within the docker container."
+docker run --rm -v $BASE:$BASE -v $BASE/.ssh:/root/.ssh $GIT_REPO \
+	/bin/bash -c " \
+	adduser -h $BASE -D -u $(id -u $USER) -s /bin/bash $USER && \
+	su - $USER -c \"cd $BASE/data/deployments/$GIT_REPO; ./build.sh $BUILD_LEVEL $VERSION $BASE/temp/build-$GIT_REPO-$DEPLOYMENT_LEVEL\""
 
 # Generate the version file.
-echo "$VERSION" > $BASE/builds/$GIT_REPO-$DEPLOYMENT_LEVEL/version.txt
+echo "$VERSION" > $BASE/temp/build-$GIT_REPO-$DEPLOYMENT_LEVEL/version.txt
 
 if [[($DEPLOYMENT_LEVEL != "local")]]; then
 	# AWS sync the files up to S3.
-	echo "Uploading the built app to the S3 folder."
-	$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-$DEPLOYMENT_LEVEL/$DEPLOYMENT_FOLDER_NAME $BASE/builds/$GIT_REPO-$DEPLOYMENT_LEVEL
+	echo "Uploading the built app to the S3 eyes-$DEPLOYMENT_LEVEL/$DEPLOYMENT_FOLDER_NAME folder."
+	$BASE/code/aws-s3-sync/sync.sh sync-s3-folder eyes-$DEPLOYMENT_LEVEL/$DEPLOYMENT_FOLDER_NAME $BASE/temp/build-$GIT_REPO-$DEPLOYMENT_LEVEL
+	if [[($ADD_VERSION == "addversion")]]; then
+		echo "Uploading the built app to the S3 eyes-$DEPLOYMENT_LEVEL/$DEPLOYMENT_FOLDER_NAME-$VERSION folder."
+		$BASE/code/aws-s3-sync/sync.sh sync-s3-folder eyes-$DEPLOYMENT_LEVEL/$DEPLOYMENT_FOLDER_NAME-$VERSION $BASE/temp/build-$GIT_REPO-$DEPLOYMENT_LEVEL
+	fi
 
 	if [[($DEPLOYMENT_LEVEL = "production")]]; then
-		$BASE/pipelines/aws-s3-sync/invalidate.py E3JMG193HISS1S "/"$DEPLOYMENT_FOLDER_NAME"/*"
+		$BASE/code/aws-s3-sync/invalidate.sh E3JMG193HISS1S "/"$DEPLOYMENT_FOLDER_NAME"/*"
+		$BASE/code/aws-s3-sync/invalidate.sh E3JMG193HISS1S "/"$DEPLOYMENT_FOLDER_NAME-$VERSION"/*"
 	fi
 else
-	echo "Syncing the built app to the server www folder."
-	if [[($DEPLOYMENT_FOLDER_NAME =~ ^(apps|(assets/static)).*)]]; then
-		mkdir -p $BASE/www/$DEPLOYMENT_FOLDER_NAME/
-		rsync -rtv --delete $BASE/builds/$GIT_REPO-$DEPLOYMENT_LEVEL/ $BASE/www/$DEPLOYMENT_FOLDER_NAME/
-	else
-		echo "If installing locally, you must choose a deployment folder that starts with \"apps\" or \"assets/static\"."
-		exit 1
+	echo "Syncing the built app to the local $BASE/data/www/$DEPLOYMENT_FOLDER_NAME folder."
+	mkdir -p $BASE/data/www/$DEPLOYMENT_FOLDER_NAME/
+	rsync -rtv --delete $BASE/temp/build-$GIT_REPO-$DEPLOYMENT_LEVEL/ $BASE/data/www/$DEPLOYMENT_FOLDER_NAME/
+	if [[($ADD_VERSION == "addversion")]]; then
+		echo "Syncing the built app to the local $BASE/data/www/$DEPLOYMENT_FOLDER_NAME-$VERSION folder."
+		mkdir -p $BASE/data/www/$DEPLOYMENT_FOLDER_NAME-$VERSION/
+		rsync -rtv --delete $BASE/temp/build-$GIT_REPO-$DEPLOYMENT_LEVEL/ $BASE/data/www/$DEPLOYMENT_FOLDER_NAME-$VERSION/
 	fi
 fi
 
