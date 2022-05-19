ee53c12d4e3e244e45888d18fd8e098e46e8abfc
Stephen O'Rourley <hurley@jpl.nasa.gov>
2022-05-19 08:35:52 -0700

    #2212 Added indenting to log function.

--- log.sh
+++ log.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
+# Set the indent to null if nothing has changed it.
+LOG_INDENT=${LOG_INDENT:-0}
+
 # Output each stdout and sterror line with a prefix of the ISO timestamp to both stdout and the given file.
 while IFS= read -r line; do
-        time=`date "+%Y-%m-%d %H:%M:%S"`
-        echo "$time $line"
+	time=`date "+%Y-%m-%d %H:%M:%S"`
+	printf "%*s%s %s" $LOG_INDENT '' "$time" "$line"
 done
-
