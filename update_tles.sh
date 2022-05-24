b6e616d43654ba0229cbb1f1a48b8a2639cee9ad
Stephen O'Rourley <hurley@jpl.nasa.gov>
2022-05-24 10:29:10 -0700

    #2212 Dockerized update_tles pipeline.

--- update_tles.sh
+++ update_tles.sh
@@ -10,67 +10,62 @@ fi
 
 # Get the base folder of the system.
 BASE=$(cd "$(dirname "$0")/../.."; pwd)
-TLE_SYNCER_DIR=$BASE/pipelines/tle_syncer
-TLE_TO_SPK_DIR=$BASE/pipelines/tle_to_spk
-AWS_S3_SYNC_DIR=$BASE/pipelines/aws-s3-sync
-TLE_DIR=$BASE/sources/tle
+TLE_SYNCER_DIR=$BASE/code/tle-syncer
+TLE_TO_SPK_DIR=$BASE/code/tle-to-spk
+AWS_S3_SYNC_DIR=$BASE/code/aws-s3-sync
+TLE_DIR=$BASE/data/tle
 CLOUDFRONT_PRODUCTION_ID=E3JMG193HISS1S
 
 # Get the needed lsk file.
-lsk_file=$(ls $BASE/sources/spice/lsk/00_lsk/naif* | tail -n1)
+lsk_file=$(ls $BASE/data/spice/lsk/00_lsk/naif* | tail -n1)
 
 echo "Updating TLEs..."
 
 # Sync the TLEs.
-$TLE_SYNCER_DIR/sync.py -o $TLE_DIR/merged_new.txt
+$TLE_SYNCER_DIR/sync.sh -o $TLE_DIR/merged_new.txt
 
 # Compare the results to the backup and generated a list of spacecraft to generate.
-tle_list=$($TLE_SYNCER_DIR/compare.py $TLE_DIR/merged.txt $TLE_DIR/merged_new.txt | sort)
+tle_list=$($TLE_SYNCER_DIR/compare.sh $TLE_DIR/merged.txt $TLE_DIR/merged_new.txt | sort)
 
 IFS=$'\n'
 for tle_name in $tle_list; do
 	if [ $tle_name == '' ]; then break; fi
-	pioneer_name=$(echo "$tle_name" | $TLE_SYNCER_DIR/names_to_eyes.py)
-	echo "  Updating $tle_name/$pioneer_name..."
-	if [ -f $BASE/pipelines/animdatagen/config/tles/$pioneer_name.json ] || [ -f $BASE/pipelines/dynamogen/config/tles/$pioneer_name.json ]; then
+	eyes_name=`$TLE_SYNCER_DIR/tle_name_to_eyes_name.sh "$tle_name"`
+	echo "  Updating $tle_name/$eyes_name..."
+	if [ -f $BASE/code/animdatagen/config/tles/$eyes_name.json ] || [ -f $BASE/code/dynamogen/config/tles/$eyes_name.json ]; then
 		# Create the spk.
 		echo "    tle -> spk"
-		mkdir -p $BASE/sources/spice/$pioneer_name
-		$TLE_TO_SPK_DIR/tle_to_spk.py -t $TLE_DIR/merged_new.txt -n "$tle_name" -l $lsk_file -o $BASE/sources/spice/$pioneer_name/out.bsp -s 0 -e 2419200
+		mkdir -p $BASE/data/spice/$eyes_name
+		$TLE_TO_SPK_DIR/tle_to_spk.sh -t $TLE_DIR/merged_new.txt -n "$tle_name" -l $lsk_file -o $BASE/data/spice/$eyes_name/out.bsp -s 0 -e 2419200
 		# Create the filename for the spk.
-		DATE=`brief $BASE/sources/spice/$pioneer_name/out.bsp | grep "^[ ]*[0-9][0-9][0-9][0-9]" | awk -F '[[:space:]][[:space:]]+' '{ print $2 }'`
-		if [ `uname -s` == 'Darwin' ]; then
-			DATE=`date -j -f "%Y %b %d %H:%M:%S" "$DATE" "+%Y%m%d_%H%M%S" 2> /dev/null`
-		else
-			DATE=`chronos -FROM UTC -TO UTC -TIME "$DATE" -SETUP $lsk_file`
-			DATE=`date -d "$DATE" "+%Y%m%d_%H%M%S" 2> /dev/null`
-		fi
-		mv $BASE/sources/spice/$pioneer_name/out.bsp $BASE/sources/spice/$pioneer_name/$DATE.bsp
+		DATE=`$TLE_TO_SPK_DIR/get_date_of_spk.sh $BASE/data/spice/$eyes_name/out.bsp $lsk_file`
+		DATE=`date -d "$DATE" "+%Y%m%d_%H%M%S" 2> /dev/null`
+		mv $BASE/data/spice/$eyes_name/out.bsp $BASE/data/spice/$eyes_name/$DATE.bsp
 		# Create and upload the animdata.
-		if [ -f $BASE/pipelines/animdatagen/config/tles/$pioneer_name.json ]; then
+		if [ -f $BASE/code/animdatagen/config/tles/$eyes_name.json ]; then
 			echo "    spk -> animdata"
-			$BASE/pipelines/animdatagen/animdatagen --spice $BASE/sources/spice --output $BASE/sources/animdata --config $BASE/pipelines/animdatagen/config/tles/$pioneer_name.json
+			$BASE/code/animdatagen/animdatagen.sh --spice $BASE/data/spice --output $BASE/data/animdata --config $BASE/code/animdatagen/config/tles/$eyes_name.json
 			echo "    Uploading animdata to bh2"
-			ssh -n -q pipeline@blackhawk2 "mkdir -p /var/server/master/spice/$pioneer_name/"
-			scp -r -p -q $BASE/sources/animdata/$pioneer_name/* pipeline@blackhawk2:/var/server/master/spice/$pioneer_name/
-			ssh -n -q pipeline@blackhawk2 "mkdir -p /var/server/staging/spice/$pioneer_name/"
-			scp -r -p -q $BASE/sources/animdata/$pioneer_name/* pipeline@blackhawk2:/var/server/staging/spice/$pioneer_name/
-			ssh -n -q pipeline@blackhawk2 "mkdir -p /var/server/production/spice/$pioneer_name/"
-			scp -r -p -q $BASE/sources/animdata/$pioneer_name/* pipeline@blackhawk2:/var/server/production/spice/$pioneer_name/
+			ssh -n -q pipeline@blackhawk2 "mkdir -p /var/server/master/spice/$eyes_name/"
+			scp -r -p -q $BASE/data/animdata/$eyes_name/* pipeline@blackhawk2:/var/server/master/spice/$eyes_name/
+			# ssh -n -q pipeline@blackhawk2 "mkdir -p /var/server/staging/spice/$eyes_name/"
+			# scp -r -p -q $BASE/data/animdata/$eyes_name/* pipeline@blackhawk2:/var/server/staging/spice/$eyes_name/
+			# ssh -n -q pipeline@blackhawk2 "mkdir -p /var/server/production/spice/$eyes_name/"
+			# scp -r -p -q $BASE/data/animdata/$eyes_name/* pipeline@blackhawk2:/var/server/production/spice/$eyes_name/
 			echo "    Uploading animdata to AWS"
-			$AWS_S3_SYNC_DIR/sync.py upload-folder eyesstage/server/spice/$pioneer_name $BASE/sources/animdata/$pioneer_name quiet
-			$AWS_S3_SYNC_DIR/sync.py upload-folder eyesstatic/server/spice/$pioneer_name $BASE/sources/animdata/$pioneer_name quiet
-			$AWS_S3_SYNC_DIR/invalidate.py $CLOUDFRONT_PRODUCTION_ID "/server/spice/"$pioneer_name"/*"
+			# $AWS_S3_SYNC_DIR/sync.sh upload-folder eyesstage/server/spice/$eyes_name $BASE/data/animdata/$eyes_name quiet
+			# $AWS_S3_SYNC_DIR/sync.sh upload-folder eyesstatic/server/spice/$eyes_name $BASE/data/animdata/$eyes_name quiet
+			# $AWS_S3_SYNC_DIR/invalidate.sh $CLOUDFRONT_PRODUCTION_ID "/server/spice/"$eyes_name"/*"
 		fi
 		# Create and upload the dynamo.
-		if [ -f $BASE/pipelines/dynamogen/config/tles/$pioneer_name.json ]; then
+		if [ -f $BASE/code/dynamogen/config/tles/$eyes_name.json ]; then
 			echo "    spk -> dynamo"
-			$BASE/pipelines/dynamogen/dynamogen --spice $BASE/sources/spice --output $BASE/sources/dynamo --config $BASE/pipelines/dynamogen/config/tles/$pioneer_name.json
+			$BASE/code/dynamogen/dynamogen.sh --spice $BASE/data/spice --output $BASE/data/dynamo --config $BASE/code/dynamogen/config/tles/$eyes_name.json
 			echo "    Uploading dynamo"
-			$AWS_S3_SYNC_DIR/sync.py sync-s3-folder eyes-dev/assets/dynamic/dynamo/$pioneer_name $BASE/sources/dynamo/$pioneer_name quiet
-			$AWS_S3_SYNC_DIR/sync.py sync-s3-folder eyes-staging/assets/dynamic/dynamo/$pioneer_name $BASE/sources/dynamo/$pioneer_name quiet
-			$AWS_S3_SYNC_DIR/sync.py sync-s3-folder eyes-production/assets/dynamic/dynamo/$pioneer_name $BASE/sources/dynamo/$pioneer_name quiet
-			$AWS_S3_SYNC_DIR/invalidate.py $CLOUDFRONT_PRODUCTION_ID "/assets/dynamic/dynamo/$pioneer_name/*"
+			$AWS_S3_SYNC_DIR/sync.sh sync-s3-folder eyes-dev/assets/dynamic/dynamo/$eyes_name $BASE/data/dynamo/$eyes_name quiet
+			# $AWS_S3_SYNC_DIR/sync.sh sync-s3-folder eyes-staging/assets/dynamic/dynamo/$eyes_name $BASE/data/dynamo/$eyes_name quiet
+			# $AWS_S3_SYNC_DIR/sync.sh sync-s3-folder eyes-production/assets/dynamic/dynamo/$eyes_name $BASE/datas/dynamo/$eyes_name quiet
+			# $AWS_S3_SYNC_DIR/invalidate.sh $CLOUDFRONT_PRODUCTION_ID "/assets/dynamic/dynamo/$eyes_name/*"
 		fi
 	fi
 done
@@ -79,13 +74,13 @@ done
 mv $TLE_DIR/merged_new.txt $TLE_DIR/merged.txt
 
 # Copy tle_syncer/names.txt to the folder.
-cp --preserve=timestamps $TLE_SYNCER_DIR/names.txt $TLE_DIR
+rsync -rtv $TLE_SYNCER_DIR/names.txt $TLE_DIR
 
 # Upload the TLE list files to AWS.
 echo "Uploading TLE list files to AWS."
-$AWS_S3_SYNC_DIR/sync.py sync-s3-folder eyes-dev/assets/dynamic/tle $TLE_DIR quiet
-$AWS_S3_SYNC_DIR/sync.py sync-s3-folder eyes-staging/assets/dynamic/tle $TLE_DIR quiet
-$AWS_S3_SYNC_DIR/sync.py sync-s3-folder eyes-production/assets/dynamic/tle $TLE_DIR quiet
-$AWS_S3_SYNC_DIR/invalidate.py $CLOUDFRONT_PRODUCTION_ID "/assets/dynamic/tle/*"
+$AWS_S3_SYNC_DIR/sync.sh sync-s3-folder eyes-dev/assets/dynamic/tle $TLE_DIR quiet
+# $AWS_S3_SYNC_DIR/sync.sh sync-s3-folder eyes-staging/assets/dynamic/tle $TLE_DIR quiet
+# $AWS_S3_SYNC_DIR/sync.sh sync-s3-folder eyes-production/assets/dynamic/tle $TLE_DIR quiet
+# $AWS_S3_SYNC_DIR/invalidate.sh $CLOUDFRONT_PRODUCTION_ID "/assets/dynamic/tle/*"
 
 echo "Complete"
