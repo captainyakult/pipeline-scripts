89124a339d8cb2794ba263a0371003bf383dfb3b
Stephen O'Rourley <hurley@jpl.nasa.gov>
2022-05-24 12:41:20 -0700

    #2212 Updated update_local_tles.

--- update_local_tles.sh
+++ update_local_tles.sh
@@ -5,12 +5,12 @@ set -eo pipefail
 
 # Get the base folder of the system.
 BASE=$(cd "$(dirname "$0")/../.."; pwd)
-TLE_SYNCER_DIR=$BASE/pipelines/tle_syncer
-TLE_DIR=$BASE/sources/tle
+TLE_SYNCER_DIR=$BASE/code/tle-syncer
+TLE_DIR=$BASE/data/tle
 
 echo "Updating local TLEs..."
 
 cp $TLE_SYNCER_DIR/local_tles.txt $TLE_DIR/local_tles.txt
-$TLE_SYNCER_DIR/update_local_tles.py $TLE_DIR/local_tles.txt
+$TLE_SYNCER_DIR/update_local_tles.sh $TLE_DIR/local_tles.txt
 
 echo "Complete"
