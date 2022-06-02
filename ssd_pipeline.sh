d8006454642e3cef32dbe1a85e16ac8116077c23
Stephen O'Rourley <hurley@jpl.nasa.gov>
2022-06-02 13:24:27 -0700

    #2212 Enabled ssd_pipeline for dev.

--- ssd_pipeline.sh
+++ ssd_pipeline.sh
@@ -7,12 +7,11 @@ set -eo pipefail
 BASE=$(cd "$(dirname "$0")/../.."; pwd)
 
 # Launch the pipeline. It will also create newly_generated_spks.txt file for creating dynamos.
-cd $BASE/pipelines/ssd-pipeline
-python3 sync.py -v -o $BASE/sources/ssd -os $BASE/sources/spice/ssd
+$BASE/code/ssd-pipeline/sync.sh -v -o $BASE/data/ssd -os $BASE/data/spice/ssd -lsk `ls $BASE/data/spice/lsk/00_lsk/*.tls | tail -n 1`
 
-if [[ -f "$BASE/sources/ssd/newly_generated_spks.txt" ]]; then
+if [[ -f "$BASE/data/ssd/newly_generated_spks.txt" ]]; then
 	# Sort and make unique the newly_generated_spks file since it may have picked up multiple runs before being used and removed.
-	sort -u -o $BASE/sources/ssd/newly_generated_spks.txt $BASE/sources/ssd/newly_generated_spks.txt
+	sort -u -o $BASE/data/ssd/newly_generated_spks.txt $BASE/data/ssd/newly_generated_spks.txt
 
 	# For every new spk in the newly_generated_spks file, generate dynamo.
 	while read F; do
@@ -24,7 +23,7 @@ if [[ -f "$BASE/sources/ssd/newly_generated_spks.txt" ]]; then
 			CA_DIST=${ITEMS[3]}
 			echo "Generating dynamo for $PIONEER_NAME..."
 			# Create the dynamo config.
-			cat > $BASE/sources/ssd/ssd.$PIONEER_NAME.sun.orb.json <<- EOM
+			cat > $BASE/data/ssd/ssd.$PIONEER_NAME.sun.orb.json <<- EOM
 			{
 				"type" : "orb",
 				"body" : $NAIF_ID,
@@ -38,8 +37,8 @@ if [[ -f "$BASE/sources/ssd/newly_generated_spks.txt" ]]; then
 			}
 			EOM
 			# Run dynamo.
-			$BASE/pipelines/dynamogen/dynamogen --spice $BASE/sources/spice --output $BASE/sources/dynamo --config $BASE/sources/ssd/ssd.$PIONEER_NAME.sun.orb.json
-			rm $BASE/sources/ssd/ssd.$PIONEER_NAME.sun.orb.json
+			$BASE/code/dynamogen/dynamogen.sh --spice $BASE/data/spice --output $BASE/data/dynamo --config $BASE/data/ssd/ssd.$PIONEER_NAME.sun.orb.json
+			rm $BASE/data/ssd/ssd.$PIONEER_NAME.sun.orb.json
 			# If the next closest approach distance is less than 0.05 AU, make earth-relative dynamo for that approach as well.
 			if (( $(echo "$CA_DIST < 7479893.53455" | bc -l) )); then
 				# Do the same for earth.
@@ -48,7 +47,7 @@ if [[ -f "$BASE/sources/ssd/newly_generated_spks.txt" ]]; then
 				START_TIME=$(echo "$CA_TIME - 604800" | bc)
 				END_TIME=$(echo "$CA_TIME + 604800" | bc)
 				# Create the dynamo config.
-				cat > $BASE/sources/ssd/ssd.$PIONEER_NAME.earth.orb.json <<- EOM
+				cat > $BASE/data/ssd/ssd.$PIONEER_NAME.earth.orb.json <<- EOM
 				{
 					"type" : "orb",
 					"body" : $NAIF_ID,
@@ -62,23 +61,23 @@ if [[ -f "$BASE/sources/ssd/newly_generated_spks.txt" ]]; then
 				}
 				EOM
 				# Run dynamo.
-				$BASE/pipelines/dynamogen/dynamogen --spice $BASE/sources/spice --output $BASE/sources/dynamo --config $BASE/sources/ssd/ssd.$PIONEER_NAME.earth.orb.json
-				rm $BASE/sources/ssd/ssd.$PIONEER_NAME.earth.orb.json
+				$BASE/code/dynamogen/dynamogen.sh --spice $BASE/data/spice --output $BASE/data/dynamo --config $BASE/data/ssd/ssd.$PIONEER_NAME.earth.orb.json
+				rm $BASE/data/ssd/ssd.$PIONEER_NAME.earth.orb.json
 			fi
 			# Sync the dynamo folder to AWS.
 			echo "  Uploading to dev, staging, and production AWS..."
-			$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-dev/assets/dynamic/dynamo/ssd/$PIONEER_NAME $BASE/sources/dynamo/ssd/$PIONEER_NAME quiet
-			$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-staging/assets/dynamic/dynamo/ssd/$PIONEER_NAME $BASE/sources/dynamo/ssd/$PIONEER_NAME quiet
-			$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-production/assets/dynamic/dynamo/ssd/$PIONEER_NAME $BASE/sources/dynamo/ssd/$PIONEER_NAME quiet
-			$BASE/pipelines/aws-s3-sync/invalidate.py E3JMG193HISS1S "/assets/dynamic/dynamo/ssd/$PIONEER_NAME/*"
-	done < $BASE/sources/ssd/newly_generated_spks.txt
+			$BASE/code/aws-s3-sync/sync.sh sync-s3-folder eyes-dev/assets/dynamic/dynamo/ssd/$PIONEER_NAME $BASE/data/dynamo/ssd/$PIONEER_NAME quiet
+			# $BASE/code/aws-s3-sync/sync.sh sync-s3-folder eyes-staging/assets/dynamic/dynamo/ssd/$PIONEER_NAME $BASE/data/dynamo/ssd/$PIONEER_NAME quiet
+			# $BASE/code/aws-s3-sync/sync.sh sync-s3-folder eyes-production/assets/dynamic/dynamo/ssd/$PIONEER_NAME $BASE/data/dynamo/ssd/$PIONEER_NAME quiet
+			# $BASE/code/aws-s3-sync/invalidate.sh E3JMG193HISS1S "/assets/dynamic/dynamo/ssd/$PIONEER_NAME/*"
+	done < $BASE/data/ssd/newly_generated_spks.txt
 fi
 
 # Delete the no longer needed newly_generated_spks file.
-rm -f $BASE/sources/ssd/newly_generated_spks.txt
+rm -f $BASE/data/ssd/newly_generated_spks.txt
 
 # Sync the ssd folder to AWS.
-$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-dev/assets/dynamic/ssd $BASE/sources/ssd quiet
-$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-staging/assets/dynamic/ssd $BASE/sources/ssd quiet
-$BASE/pipelines/aws-s3-sync/sync.py sync-s3-folder eyes-production/assets/dynamic/ssd $BASE/sources/ssd quiet
-$BASE/pipelines/aws-s3-sync/invalidate.py E3JMG193HISS1S "/assets/dynamic/ssd/*"
+$BASE/code/aws-s3-sync/sync.py sync-s3-folder eyes-dev/assets/dynamic/ssd $BASE/data/ssd quiet
+# $BASE/code/aws-s3-sync/sync.py sync-s3-folder eyes-staging/assets/dynamic/ssd $BASE/data/ssd quiet
+# $BASE/code/aws-s3-sync/sync.py sync-s3-folder eyes-production/assets/dynamic/ssd $BASE/data/ssd quiet
+# $BASE/code/aws-s3-sync/invalidate.py E3JMG193HISS1S "/assets/dynamic/ssd/*"
