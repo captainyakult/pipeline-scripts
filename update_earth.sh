80417e2c08ce9ff427cffbddaa679f9862fd637a
pipeline <pipeline@blackhawk3>
2022-06-24 06:29:50 -0700

    #2212 Dockerized update_earth.sh

--- update_earth.sh
+++ update_earth.sh
@@ -3,38 +3,37 @@
 # Fail on any error.
 set -eo pipefail
 
-API='https://climate-4.client.mooreboeck.com'
+API='https://climate-vtad.client.mooreboeck.com'
 
 # Get the base folder of the system.
 BASE=$(cd "$(dirname "$0")/../.."; pwd)
-DIR=$BASE'/sources/earth-api-dev'
-AWS_S3_SYNC_DIR=$BASE/pipelines/aws-s3-sync
+DIR=$BASE'/data/earth-api-dev'
+AWS_S3_SYNC_DIR=$BASE/code/aws-s3-sync
 DIST='eyes-dev/assets/dynamic/earth/api'
 CLOUDFRONT_PRODUCTION_ID=E3JMG193HISS1S
 
 if [[ ($1 == 'production') ]]; then
-    DIR=$BASE'/sources/earth-api'
+    DIR=$BASE'/data/earth-api'
     DIST='eyes-production/assets/dynamic/earth/api'
     API='https://climate.nasa.gov'
 fi
 
+echo "Getting Earth Now Vital Signs"
 VITAL_URL=${API}'/api/v1/earth_now_vital_signs/?category=116'
-echo VITAL_URL $VITAL_URL
 curl -sS $VITAL_URL > $DIR/vital.json
 
+echo "Getting Earth Now Events"
 EVENT_URL=${API}'/api/v1/earth_now_events/?order=date+desc&per_page=-1'
-echo EVENT_URL $EVENT_URL
 curl -sS $EVENT_URL > $DIR/events.json
 
+echo "Getting Earth Missions"
 MISSION_URL=${API}'/api/v1/missions/?category=133&order=position&per_page=-1'
-echo MISSION_URL $MISSION_URL
 curl -sS $MISSION_URL > $DIR/mission.json
 
 # Upload the files to AWS.
-echo 'syncing '$DIR' to '$DIST
-$AWS_S3_SYNC_DIR/sync.py sync-s3-folder $DIST $DIR
+$AWS_S3_SYNC_DIR/sync.sh sync-s3-folder $DIST $DIR
 
 if [[ ($1 == 'production') ]]; then
-    $AWS_S3_SYNC_DIR/invalidate.py $CLOUDFRONT_PRODUCTION_ID "/assets/dynamic/earth/api/*"
+    $AWS_S3_SYNC_DIR/invalidate.sh $CLOUDFRONT_PRODUCTION_ID "/assets/dynamic/earth/api/*"
 fi
 
